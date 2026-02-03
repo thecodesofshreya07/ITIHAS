cc926c6c16b71821205cfa695a4af26afd1138a0
Shreya Mishra <sm8054800@gmail.com>
2026-02-03 22:02:28 +0530

    Initial ITIHAS setup with Vite and homepage placeholders

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header/Header";
+import Map from "../components/Map/Map";
+import Timeline from "../components/Timeline/Timeline";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Timeline />
+      <Map />
+    </>
+  );
+}
+
