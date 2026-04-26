a539f88a5bbd08dd9e91d0ba9a001469b6dcc778
Shreya Mishra <sm8054800@gmail.com>
2026-04-27 00:22:30 +0530

    api 2

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const eventRoutes = require('./routes/events');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://itihas-one.vercel.app/', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
